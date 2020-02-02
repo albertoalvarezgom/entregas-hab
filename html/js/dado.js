812cd47a130eedd2f62764098a79e5a4cfc61a9d
albertoalvarezgom <albertoalvarezgom@gmail.com>
2020-02-02 18:21:38 +0100

    Actualizado ejercicio del dado

--- html/js/dado.js
+++ html/js/dado.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 function dice() {
   const diceNum = Math.ceil(Math.random() * 6);
@@ -7,8 +7,16 @@ function dice() {
 
 let totalScore = 0;
 
-for (let i = 1; totalScore <= 50; i++) {
+for (let i = 1; totalScore < 50; i++) {
   const diceNum = dice();
   totalScore += diceNum;
-  console.log(`Tirada ${i}: ha salido un ${diceNum}. Tienes un total de ${totalScore} puntos.`);
+  if (totalScore < 50) {
+    console.log(
+      `Tirada ${i}: ha salido un ${diceNum}. Tienes un total de ${totalScore} puntos.`
+    );
+  } else {
+    console.log(
+      `¡Enhorabuena, ha salido un ${diceNum}!¡Has ganado con un total de ${totalScore} puntos!`
+    );
+  }
 }
